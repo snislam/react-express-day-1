638b99644cd62d2e6c1f551e4d22d356f40e8e5f
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-04-21 23:14:49 +0600

    First day work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [users])
+
+  const handlePostData = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    // send to server
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, user];
+        setUsers(newUsers);
+        console.log(data);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am in practise mood.</h1>
+      <p>{users.length}</p>
+      <div>
+        <form onSubmit={handlePostData}>
+          <input type="text" name="name" id="" placeholder='Name' required /> <br /> <br />
+          <input type="email" name="email" id="" placeholder='Email' required /> <br /> <br />
+          <input type="submit" value="Send to server" />
+        </form>
+      </div>
+      <div>
+        {
+          users.map(user => <li key={user.id}>{user.id}, {user.name}, {user.email}</li>)
+        }
+      </div>
     </div>
   );
 }
